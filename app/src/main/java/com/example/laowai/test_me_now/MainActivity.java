ac41407d339e1fe2c12befd92e78b0da4db97b1b
unknown <skipbox500@gmail.com>
2018-02-11 00:40:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/laowai/test_me_now/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.laowai.test_me_now;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
